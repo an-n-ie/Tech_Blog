4c0ab71faea3bae9e1a51242d1449fb1e1226b28
Annie Hoang <anniehoang.anh@gmail.com>
2022-09-07 22:19:55 -0400

    her7

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -9,6 +9,7 @@ router.post('/', withAuth, async (req, res) => {
       user_id: req.session.user_id,
     });
 
+    
     res.status(200).json(newProject);
   } catch (err) {
     res.status(400).json(err);
